5af8a0836e14eb9fc81e56227a129fc3a5b845ba
spv <spv@vt2b.ru>
2025-07-15 16:20:45 +0500

    feat: удален app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { App } from './app';
 export { TrieNode } from './interface/trie-node.interface';
 export { TrieService } from './services/trie.service';
 export { TrieController } from './controllers/trie.controller';
-
-const app = new App();
-app.demo();
