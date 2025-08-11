bd675f8578a4e2d4d463fa1d03c82cebf8b08b26
spv <spv@vt2b.ru>
2025-08-11 13:12:20 +0500

    v1.0.6

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser'; // Добавьте этот импорт
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -10,13 +10,13 @@ export default {
       file: 'dist/index.cjs.js',
       format: 'cjs',
       exports: 'auto',
-      plugins: [terser()], // Добавьте минификацию здесь
+      plugins: [terser()],
     },
     {
       file: 'dist/index.esm.js',
       format: 'es',
       exports: 'auto',
-      plugins: [terser()], // И здесь
+      plugins: [terser()],
     },
   ],
   plugins: [
